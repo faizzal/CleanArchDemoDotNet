67e1dcd24c3ad548b2e3c9ae51664a72ecf392cf
Faizal <Faizal@DESKTOP-FS3PIJH>
2020-01-31 20:06:48 +0300

    add employee model

---
+++ CleanArch/ClearArch.Domain/Models/Employee.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+namespace ClearArch.Domain.Models
+{ 
+    [Table("Arch_INT_Employee")]
+    public class Employee
+    {
+
+        public int ID { get; set; }
+        [Required]
+        [StringLength(50)]
+        [Display(Name = "Last Name")]
+        [Column("Last_Name"), DataType("varchar(max)")]
+
+        public string LastName { get; set; }
+        [Required]
+        [StringLength(50, ErrorMessage = "First name cannot be longer than 50 characters.")]
+        [Column("First_Name")]
+        [Display(Name = "First Name")]
+        public string FirstMidName { get; set; }
+        [DataType(DataType.Date)]
+        [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}", ApplyFormatInEditMode = true)]
+        [Column("Enrollment_Date")]
+        [Display(Name = "Enrollment Date")]
+        public DateTime EnrollmentDate { get; set; }
+        [Column("Full_Name")]
+        [Display(Name = "Full Name")]
+        public string FullName
+        {
+            get
+            {
+                return LastName + ", " + FirstMidName;
+            }
+        }
+        [Column("Salary")]
+
+        public double Salary { get; set; }
+    }
+}
